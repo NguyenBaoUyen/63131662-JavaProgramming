8859a27efd94521765f5c6c32bbf01c0f7f7e95f
NguyenBaoUyen <145897661+NguyenBaoUyen@users.noreply.github.com>
2024-01-19 20:44:07 +0700

    Ví dụ đầu tiên
    chỉ để Demo

--- Bai1_Hello/src/SayHelloApp.java
+++ Bai1_Hello/src/SayHelloApp.java
@@ -13,7 +13,7 @@ public class SayHelloApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xin chao. Toi la Nguyen Bao Uyen");
-
+		System.out.print("TÔI LẦN ĐẦU HỌC JAVA, và đang quản lý mã nguồn với GIT");
 	}
 
 }
